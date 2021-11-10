c2af6961fc15b2ef422b73542ac8fdb702f73f98
tjwjdgks <tjwjdgks43@gmail.com>
2021-11-10 10:07:25 +0900

    문제점 있는 코드 최종

--- src/com/company/chapter1/TicketSeller.java
+++ src/com/company/chapter1/TicketSeller.java
@@ -1,5 +1,13 @@
 package com.company.chapter1;
 
 public class TicketSeller {
+    private TicketOffice ticketOffice;
 
+    public TicketSeller(TicketOffice ticketOffice){
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
 }
